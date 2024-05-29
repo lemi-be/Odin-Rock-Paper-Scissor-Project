55e8ee1736aba298007f85f47157c2dac6bba0a8
Lemi Bekele <lemibekele365@gmail.com>
2024-05-29 22:14:33 +0300

    added functions to script.js first function to display welcome message second function to ask user for input

---
+++ script.js
@@ -0,0 +1,15 @@
+// make welcoming message
+// set Function promptMessage
+function promptMessage() {
+  // dispaly welcome message
+  console.log("Welcome to Rock Paper Message game!!");
+}
+
+// Asking user to choose
+// set function promptUser
+function promptMessage() {
+  // set variable to store users choice
+  let userChoice = prompt("Choose one (Rock, Paper, Scissors)");
+  return userChoice;
+}
+
