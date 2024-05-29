14b65f9816e793a130c42a77598efb47ae8be439
Lemi Bekele <lemibekele365@gmail.com>
2024-05-29 23:02:17 +0300

    edited perviously added functions added new function called computerChoice

--- script.js
+++ script.js
@@ -1,15 +1,25 @@
 // make welcoming message
 // set Function promptMessage
 function promptMessage() {
-  // dispaly welcome message
+  // display welcome message
   console.log("Welcome to Rock Paper Message game!!");
 }
 
 // Asking user to choose
-// set function promptUser
-function promptMessage() {
-  // set variable to store users choice
-  let userChoice = prompt("Choose one (Rock, Paper, Scissors)");
-  return userChoice;
+// set function userChoice
+function userChoice() {
+  // set variable to store users input
+  let userInput = prompt("Choose one (Rock, Paper, Scissors)");
+  return userInput;
+}
+
+// Picking the computer choice
+// set function computerChoice
+function computerChoice() {
+  // set an array to store a list for the computer to pick from
+  const prompts = ["Rock", "Paper", "Scissors"];
+  // set a constant to store the randomly picked choice
+  const randomPrompts = prompts[Math.floor(Math.random() * prompts.length)];
+  return randomPrompts;
 }
 
